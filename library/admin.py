73770eb8139e8419e867e2c4e8683cd61dd45851
alexgrodetskiy <oleksiy.grodetsky@gmail.com>
2025-02-05 20:52:44 +0200

    Adding an administrator interface with custom ModelAdmin classes

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import MediaItem, UserItem
 
-# Register your models here.
+
+@admin.register(MediaItem)
+class MediaItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'media_type', 'release_year')
+    list_filter = ('media_type',)
+    search_fields = ('title',)
+
+
+@admin.register(UserItem)
+class UserItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'media_item', 'status', 'updated_on')
+    list_filter = ('status',)
+    search_fields = ('user__username', 'media_item__title')

6c35081ef8b219f4bc3e83e94914feda49471c78
alexgrodetskiy <oleksiy.grodetsky@gmail.com>
2025-06-23 11:20:00 +0300

    Refactor authentication forms and improve login/sign up UI

--- library/forms.py
+++ library/forms.py
@@ -9,3 +9,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].help_text = None
